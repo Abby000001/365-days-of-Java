d9bcb3a0c74e416f7eb8bb73f73163d4cb30561a
Abby000001 <abhishekrawat8034@gmail.com>
2025-06-17 14:14:06 +0530

    Day 1: Hello World in Java

--- Day01/HelloWorld.java
+++ Day01/HelloWorld.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Day 1: Hello GitHub 365-Day Challenge!");
+public class HelloWorld { //Yeh ek class bana rahe ho jiska naam hai HelloWorld. Java mein sab kuch class ke andar likhte hain. public ka matlab yeh class sab jagah accessible hai.
+    public static void main(String[] args) { //public: sab jagah se accessible static: bina object banaye chal sakta hai void: kuch return nahi karega String[] args: command-line se input le sakte ho
+        System.out.println("Day 1: Hello GitHub 365-Day Challenge!"); //System.out.println(...) – Yeh line console pe message print karegi
     }
 }
