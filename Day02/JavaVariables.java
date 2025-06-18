b0c6a6621286f182fbf9167008b60eaf6db0c42c
Abby000001 <abhishekrawat8034@gmail.com>
2025-06-18 22:12:35 +0530

    Day 2: Java Variables and Data Types

---
+++ Day02/JavaVariables.java
@@ -0,0 +1,15 @@
+public class JavaVariables {
+    public static void main(String[] args) {
+        int age = 22;
+        double height = 5.9;
+        char grade = 'A';
+        boolean isJavaFun = true;
+        String name = "Abhishek";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Loves Java? " + isJavaFun);
+    }
+}
